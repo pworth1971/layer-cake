37a3a4e68f7550e131a17cd15e3cef68d8922d14
Peter J Worth <peterworthjr@gmail.com>
2024-06-29 12:07:26 +0000

    updated run)newsgroups.sh for fastText

--- bin/run_newsgroups.sh
+++ bin/run_newsgroups.sh
@@ -13,17 +13,17 @@ do
 
 dataset="--dataset 20newsgroups"
 #$PY $LOG $dataset	$CNN	--learnable 200	--channels 256 --seed $run
-$PY $LOG $dataset	$CNN	--channels 128	--pretrained glove --glove-path $EMB --seed $run
+#$PY $LOG $dataset	$CNN	--channels 128	--pretrained glove --glove-path $EMB --seed $run
 #$PY $LOG $dataset	$CNN	--channels 128	--pretrained word2vec --word2vec-path $EMB/GoogleNews-vectors-negative300.bin --seed $run
 
 #$PY $LOG $dataset	$LSTM	--learnable 200	--hidden 256 --seed $run
-$PY $LOG $dataset	$LSTM	--hidden 256	--pretrained glove --glove-path $EMB --seed $run
+#$PY $LOG $dataset	$LSTM	--hidden 256	--pretrained glove --glove-path $EMB --seed $run
 #$PY $LOG $dataset	$LSTM	--hidden 256	--pretrained word2vec --word2vec-path $EMB/GoogleNews-vectors-negative300.bin --seed $run
 
 #$PY $LOG $dataset	$ATTN	--learnable 200	--hidden 256 --seed $run
-$PY $LOG $dataset	$ATTN	--hidden 256	--pretrained glove --glove-path $EMB --seed $run
+#$PY $LOG $dataset	$ATTN	--hidden 256	--pretrained glove --glove-path $EMB --seed $run
 #$PY $LOG $dataset	$ATTN	--hidden 256	--pretrained word2vec --word2vec-path $EMB/GoogleNews-vectors-negative300.bin --seed $run
 
-common="--dataset-dir ../fasttext/dataset --log-file ../log/fasttext.10runs.csv"
+common="--dataset-dir ../fasttext/dataset --log-file ../log/run_newsgroups.fasttext"
 python ../src/fasttext.py --dataset 20newsgroups	--learnable 50	--lr 0.5	--nepochs 50	--seed $run --pickle-path ../pickles/20newsgroups.pickle
 done
