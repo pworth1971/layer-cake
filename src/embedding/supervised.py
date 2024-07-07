fixed zscores computation for embedding matrix (normalization)

src/embedding/supervised.py
@@ -21,18 +21,62 @@ from data.tsr_function__ import  STWFUNCTIONS
 
 
 # ----------------------------------------------------------------------------------------------------------------------
-# zscores: 
-# ----------------------------
-"""
-Normalizes an array X using z-score normalization.
+# normalize_zscore: 
+#
+# Function to compute z-scores for each feature across all samples. Replacement for 
+# zscores() below, which is not working with the new numpy libraries.
+#
+#
+# Parameters:
+#       data (numpy.ndarray): The input data array with shape (n_samples, n_features).
+# 
+# Returns:
+#       numpy.ndarray: Z-score normalized data array.
+# ----------------------------------------------------------------------------------------------------------------------
 
-Implementation: The standard deviation is calculated with a minimum clip value to prevent division by zero. 
-This normalized form ensures each feature (column if axis=0) has zero mean and unit variance.
-"""
-def zscores(x, axis=0): #scipy.stats.zscores does not avoid division by 0, which can indeed occur
-    std = np.clip(np.std(x, ddof=1, axis=axis), 1e-5, None)
-    mean = np.mean(x, axis=axis)
-    return (x - mean) / std
+def normalize_zscores(data):
+    
+    print("--- normalize_zscores() ---")
+    print("data ", {data.shape})
+
+    means = np.mean(data, axis=0)       # Mean of the data (computing along the rows: axis=0)
+    stds = np.std(data, axis=0)         # Standard deviation of the data (computing along the rows: axis=0) 
+    z_scores = (data - means) / stds    # Compute the z-scores: (x - mean) / std
+    
+    return z_scores
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# zscores:
+#
+# Normalizes an array X using z-score normalization.
+
+# Implementation: The standard deviation is calculated with a minimum clip value 
+# to prevent division by zero. This normalized form ensures each feature (column if axis=0) 
+# has zero mean and unit variance.
+#  
+# ----------------------------------------------------------------------------------------------------------------------
+def zscores(x, axis=0):                             #scipy.stats.zscores does not avoid division by 0
+    
+    print("\t--- zscores() ---")
+    print("x shape:", {x.shape})
+    print("x dtype:", x.dtype)              # Check data type
+    print("axis: ", {axis})
+
+    arrX = x.todense(x)
+    print("arrX shape:", {arrX.shape})
+    
+    np_std = np.std(arrX, ddof=1, axis=axis)
+    std = np.clip(np_std, 1e-5, None)
+
+    #std = np.clip(np.std(x, ddof=1, axis=axis), 1e-5, None)
+    #mean = np.mean(x, axis=axis)
+    mean = np.mean(arrX, axis=axis)
+    
+    #return (x - mean) / std
+    return (arrX - mean) / std
+# ----------------------------------------------------------------------------------------------------------------------
 
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -112,9 +156,14 @@ applies z-score normalization and possibly PCA for dimensionality reduction if t
 label space exceeds a defined limit.
 """
 def get_supervised_embeddings(X, Y, max_label_space=300, binary_structural_problems=-1, method='dotn', dozscore=True):
+
+    print("----- get_supervised_embeddings() -----")
+
     nC = Y.shape[1]
     if nC==2 and binary_structural_problems > nC:
         raise ValueError('not implemented in this branch')
+
+    print("method: ", {method})
 
     if method=='ppmi':
         F = supervised_embeddings_ppmi(X, Y)
@@ -129,8 +178,13 @@ def get_supervised_embeddings(X, Y, max_label_space=300, binary_structural_probl
     elif method == 'wp':
         F = supervised_embeddings_tsr(X, Y, word_prob)
 
+    print("F:", {F.shape})
+
     if dozscore:
-        F = zscores(F, axis=0)
+        #F = zscores(F, axis=0)
+        F = normalize_zscores(F)
+
+    print("after zscore normalization:", {F.shape})
 
     if max_label_space!=-1 and nC > max_label_space:
         print(f'supervised matrix has more dimensions ({nC}) than the allowed limit {max_label_space}. '

src/layer_cake.py
@@ -333,12 +333,13 @@ def main(opt):
         print('Training complete: testing')
         test_loss = test(model, test_index, yte, pad_index, dataset.classification_type, tinit, epoch, logfile, criterion, 'final-te', loss_history)
 
-    # Plot the training and testing loss after all epochs
-    plot_loss_over_epochs( opt.dataset, {
-        'epochs': np.arange(1, len(loss_history['train_loss']) + 1),
-        'train_loss': loss_history['train_loss'],
-        'test_loss': loss_history['test_loss']
-    }, method_name, '../output')
+
+    if (opt.plotmode):                                          # Plot the training and testing loss after all epochs
+        plot_loss_over_epochs( opt.dataset, {
+            'epochs': np.arange(1, len(loss_history['train_loss']) + 1),
+            'train_loss': loss_history['train_loss'],
+            'test_loss': loss_history['test_loss']
+        }, method_name, '../output')
 
 # end main() ----------------------------------------------------------------------------------------------------------------------------
 
@@ -390,7 +391,9 @@ def train(model, train_index, ytr, pad_index, tinit, logfile, criterion, optim, 
             print(f'{opt.dataset} {method_name} Epoch: {epoch}, Step: {idx}, Training Loss: {interval_loss:.6f}')
 
     mean_loss = np.mean(interval_loss)
+    
     loss_history['train_loss'].append(mean_loss)
+
     logfile.add_layered_row(epoch=epoch, measure='tr_loss', value=mean_loss, timelapse=time() - tinit)
 
     return mean_loss
@@ -451,7 +454,6 @@ def test(model, test_index, yte, pad_index, classification_type, tinit, epoch, l
 
 def set_method_name(opt):
     method_name = opt.net
-
     if opt.pretrained:
         method_name += f'-{opt.pretrained}'
     if opt.learnable > 0:

src/svm_baselines.py
@@ -129,7 +129,7 @@ def embedding_matrix(dataset, pretrained=False, supervised=False):
     vocabulary = np.asarray(list(zip(*sorted(vocabulary.items(), key=lambda x: x[1])))[0])
 
     print()
-    print('[embedding matrix]')
+    print('svm_baseline::embedding_matrix()')
     
     pretrained_embeddings = []
 
@@ -149,6 +149,7 @@ def embedding_matrix(dataset, pretrained=False, supervised=False):
     if supervised:
         
         print('\t[supervised-matrix]')
+        
         Xtr, _ = dataset.vectorize()
         Ytr = dataset.devel_labelmatrix
 
@@ -363,9 +364,9 @@ def main(args):
 
     tend += sup_tend
 
-    logfile.add_layered_row(measure='te-macro-F1', value=Mf1, timelapse=tend)
-    logfile.add_layered_row(measure='te-micro-F1', value=mf1, timelapse=tend)
-    logfile.add_layered_row(measure='te-accuracy', value=acc, timelapse=tend)
+    logfile.add_layered_row(epoch=0, tunable=False, run=0, measure='te-macro-F1', value=Mf1, timelapse=tend)
+    logfile.add_layered_row(epoch=0, tunable=False, run=0, measure='te-micro-F1', value=mf1, timelapse=tend)
+    logfile.add_layered_row(epoch=0, tunable=False, run=0, measure='te-accuracy', value=acc, timelapse=tend)
 
     print('Done!')
 