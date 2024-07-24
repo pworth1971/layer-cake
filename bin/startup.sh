001ad672ed7cd922e6079245f5275c280a6272b2
Peter J Worth <peterworthjr@gmail.com>
2024-07-24 21:57:20 +0000

    Some startup script updates for LLaMA support (large models and large datasets)

--- bin/startup.sh
+++ bin/startup.sh
@@ -3,7 +3,7 @@
 conda init
 conda create -n python38 python=3.8
 conda activate python3.8
-pip install torch torchtext scikit-learn fasttext transformers simpletransformers rdflib gensim fasttext matplotlib tabulate scipy datetime numpy pandas 
-
+pip install scikit-learn fasttext transformers simpletransformers rdflib gensim fasttext matplotlib tabulate scipy datetime numpy pandas 
+conda install -y torchtext -c pytorch
 apt update
 apt install zip
