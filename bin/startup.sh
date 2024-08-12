68513002771beb779f3b54a5c3f6cf303823af15
Peter J Worth <peterworthjr@gmail.com>
2024-08-12 10:28:39 -0400

    updated results_analysis code, updated command line args and output

--- bin/startup.sh
+++ bin/startup.sh
@@ -3,7 +3,7 @@
 conda init
 conda create -n python38 python=3.8
 conda activate python38
-pip install scikit-learn fasttext transformers simpletransformers rdflib gensim fasttext matplotlib tabulate scipy datetime numpy pandas psutil GPUtil seaborn plotly
+pip install scikit-learn fasttext transformers simpletransformers rdflib gensim fasttext matplotlib tabulate scipy datetime numpy pandas psutil GPUtil plotly
 conda install pytorch torchtext cudatoolkit=11.8 -c pytorch -c nvidia
 apt update
 apt install zip
--- src/results_analysis.py
+++ src/results_analysis.py
@@ -1,19 +1,11 @@
 import pandas as pd
 import sys
 from tabulate import tabulate
-import matplotlib.pyplot as plt
 import os
-import seaborn as sns
 import plotly.express as px
+import argparse
 
 
-def read_file(file_path):
-
-    df = pd.read_csv(file_path, sep='\t')  # Load data from a CSV file (tab delimited)
-
-    print("Columns in the file:", df.columns)
-
-    return df
 
 
 
@@ -23,9 +15,11 @@ def read_file(file_path):
 # analyze the model performance results, print summary either to sdout or file
 # ----------------------------------------------------------------------------------------------------------------------------
 
-def results_analysis(file_path, output_path=None):
+def results_analysis(df, output_path='../out'):
 
-    df = read_file(file_path=file_path)
+    # Create output directory if it doesn't exist
+    if output_path and not os.path.exists(output_path):
+        os.makedirs(output_path)
 
     # Group data by 'dataset', 'embedding', 'model', 'wc-supervised', 'measure' and get the maximum 'value'
     result = df.groupby(['dataset', 'embeddings', 'model', 'wc_supervised', 'measure'])['value'].max().reset_index()
@@ -76,120 +70,22 @@ def results_analysis(file_path, output_path=None):
 
     # Generate output
     if output_path:
-        with open(output_path, 'w') as f:
+        output_file = os.path.join(output_path, "results_analysis.out")
+        with open(output_file, 'w') as f:
             f.write(final_formatted_table)
-        print(f"Output saved to {output_path}")
+        print(f"Output saved to {output_file}")
     else:
         print(final_formatted_table)
 
+
+# ----------------------------------------------------------------------------------------------------------------------------
+# generate_charts_plotly()
+#
+# Generate plots for models and their performance evalutaion metrics and save to file 
+# ----------------------------------------------------------------------------------------------------------------------------
+
+def generate_charts_plotly(df, output_path='../out', show_charts=False):
     
-def generate_charts_seaborn(file_path, output_path=None):
-    df = read_file(file_path=file_path)
-
-    # List of measures to analyze
-    measures = ['final-te-macro-F1', 'final-te-micro-F1', 'te-hamming-loss', 'te-jacard-index']
-
-    # Create output directory if it doesn't exist
-    if output_path and not os.path.exists(output_path):
-        os.makedirs(output_path)
-
-    # Define a vibrant and distinct color palette
-    color_palette = sns.color_palette("husl", n_colors=10)  # 'husl' palette is vibrant and better spaced
-
-    for measure in measures:
-        for supervised in sorted(df['wc_supervised'].unique(), reverse=True):
-            plt.figure(figsize=(14, 7))  # Adjust the figure size
-
-            # Filter the DataFrame for the current measure and wc_supervised status
-            subset_df = df[(df['measure'] == measure) & (df['wc_supervised'] == supervised)]
-
-            # Check if the subset DataFrame is empty
-            if subset_df.empty:
-                print(f"No data available for {measure} with wc_supervised={supervised}")
-                continue
-
-            # Create the plot using Seaborn's barplot
-            sns.barplot(data=subset_df, x='model', y='value', hue='embeddings', palette=color_palette,
-                        ci=None, edgecolor='gray', linewidth=0.5)
-
-            plt.title(f'Comparison of {measure} by Model and Embeddings - {"Supervised" if supervised else "Not Supervised"}')
-            plt.ylabel('Value')
-            plt.xlabel('Model')
-            plt.legend(title="Embeddings", loc='upper right', bbox_to_anchor=(1.15, 1), borderaxespad=0.)
-
-            # Save each plot in the specified output directory
-            if output_path:
-                plot_file_name = f"{measure}_{'supervised' if supervised else 'unsupervised'}_comparison_seaborn.png"
-                plot_file = os.path.join(output_path, plot_file_name)
-                plt.savefig(plot_file, bbox_inches='tight')  # Ensure everything is included in the saved image
-                print(f"Saved plot for {measure} at {plot_file}")
-
-            # Show plot
-            plt.show()
-
-            plt.close()
-
-
-def generate_charts(file_path, output_path=None):
-
-    df = read_file(file_path=file_path)
-
-    # List of measures to analyze
-    measures = ['final-te-macro-F1', 'final-te-micro-F1', 'te-hamming-loss', 'te-jacard-index']
-
-    # Define colors for differentiation
-    colors = ['lightblue', 'lightgreen', 'salmon', 'wheat', 'lightgrey', 'cyan', 'magenta', 'yellow', 'orange', 'lime']
-
-    # Create output directory if it doesn't exist
-    if output_path and not os.path.exists(output_path):
-        os.makedirs(output_path)
-
-    for measure in measures:
-        fig, ax = plt.subplots(figsize=(14, 7))  # Adjust the figure size
-
-        legend_labels = []
-
-        for idx, supervised in enumerate([False, True]):  # Ensure False is first for consistent order
-            # Filter the DataFrame for the current measure and wc_supervised status
-            measure_df = df[(df['measure'] == measure) & (df['wc_supervised'] == supervised)]
-
-            # Check if the measure data is empty
-            if measure_df.empty:
-                print(f"No data available for {measure} with wc_supervised={supervised}")
-                continue
-
-            # Group data by 'model' and 'embeddings' and calculate the average of the 'value'
-            result = measure_df.groupby(['model', 'embeddings'])['value'].mean().unstack()
-
-            # Plotting side by side within the same subplot
-            bars = result.plot(kind='bar', ax=ax, legend=False, color=colors[idx*len(colors)//2:(idx+1)*len(colors)//2], position=idx, width=0.35)
-
-            # Append legend labels for each embedding type
-            for embedding in result.columns:
-                legend_labels.append(f'{embedding} ({"Supervised" if supervised else "Not Supervised"})')
-
-        ax.set_title(f'Comparison of {measure} by Model and Embeddings')
-        ax.set_ylabel('Value')
-        ax.set_xlabel('Model')
-
-        # Set custom legend on the left side outside the plot
-        ax.legend(legend_labels, title="Embeddings and Supervision", bbox_to_anchor=(-0.15, 0.5), loc='center left', fontsize='small', borderaxespad=0.)
-
-        # Save each plot in the specified output directory
-        if output_path:
-            plot_file = os.path.join(output_path, f"{measure}_comparison.png")
-            plt.savefig(plot_file, bbox_inches='tight')  # Ensure everything is included in the saved image
-            print(f"Saved plot for {measure} at {plot_file}")
-
-        # Show plot
-        plt.show()
-
-        plt.close()
-
-
-def generate_charts_plotly(file_path, output_path=None):
-    df = read_file(file_path=file_path)
-
     # Filter to only include specific measures
     measures = ['final-te-macro-F1', 'final-te-micro-F1']
     df = df[df['measure'].isin(measures)]
@@ -202,59 +98,106 @@ def generate_charts_plotly(file_path, output_path=None):
     color_palette = px.colors.qualitative.Safe
 
     for measure in measures:
-        for supervised in sorted(df['wc_supervised'].unique(), reverse=True):
-            # Filter the DataFrame for the current measure and wc_supervised status
-            subset_df = df[(df['measure'] == measure) & (df['wc_supervised'] == supervised)]
 
-            # Check if the subset DataFrame is empty
-            if subset_df.empty:
-                print(f"No data available for {measure} with wc_supervised={supervised}")
-                continue
+        for dataset in df['dataset'].unique():  # Loop through each dataset
+        
+            for supervised in sorted(df['wc_supervised'].unique(), reverse=True):
+                # Filter the DataFrame for the current measure, dataset, and wc_supervised status
+                subset_df = df[(df['measure'] == measure) & (df['dataset'] == dataset) & (df['wc_supervised'] == supervised)]
 
-            # Aggregate to find the maximum value per model and embeddings
-            max_df = subset_df.groupby(['model', 'embeddings']).agg({'value': 'max'}).reset_index()
+                # Check if the subset DataFrame is empty
+                if subset_df.empty:
+                    print(f"No data available for {measure} in dataset {dataset} with wc_supervised={supervised}")
+                    continue
 
-            # Create the plot using Plotly Express
-            fig = px.bar(max_df, x='model', y='value', color='embeddings', barmode='group',
-                         title=f'Maximum Value Comparison of {measure} by Model and Embeddings - {"Supervised" if supervised else "Not Supervised"}',
-                         labels={"value": "Max Value", "model": "Model"},
-                         color_discrete_sequence=color_palette,
-                         hover_data=['model', 'embeddings'])
+                # Aggregate to find the maximum value per model and embeddings
+                max_df = subset_df.groupby(['model', 'embeddings']).agg({'value': 'max'}).reset_index()
 
-            fig.update_layout(legend_title_text='Embeddings')
-            fig.update_xaxes(title_text='Model')
-            fig.update_yaxes(title_text='Maximum Measure Value', range=[0, max_df['value'].max() * 1.1])  # Adjust the y-axis range to fit max value
+                title_text = f'Dataset: {dataset.upper()}; Measure: {measure} [by Model and Embeddings Type {"(pretrained+supervised)" if supervised else "(pretrained)"}]'
 
-            # Save each plot in the specified output directory and show it
-            if output_path:
-                plot_file_name = f"max_{measure}_{'supervised' if supervised else 'unsupervised'}_comparison_plotly.html"
-                plot_file = os.path.join(output_path, plot_file_name)
-                fig.write_html(plot_file)  # Save as HTML to retain interactivity
-                print(f"Saved interactive plot for {measure} at {plot_file}")
+                # Create the plot using Plotly Express
+                fig = px.bar(max_df, x='model', y='value', color='embeddings', barmode='group',
+                             title=title_text,
+                             labels={"value": "Max Value", "model": "Model"},
+                             color_discrete_sequence=color_palette,
+                             hover_data=['model', 'embeddings'])
 
-            fig.show()  # This will display the plot in the notebook or a web browser
+                # Update layout for title
+                fig.update_layout(
+                    title={
+                        'text': title_text,
+                        'y': 0.95,
+                        'x': 0.5,
+                        'xanchor': 'center',
+                        'yanchor': 'top',
+                        'font': dict(
+                            family="Arial",
+                            size=16,
+                            color='black',
+                            weight='bold'
+                        )
+                    },
+                    legend_title_text='Embeddings'
+                )
+
+                fig.update_xaxes(title_text='Model')
+                fig.update_yaxes(title_text='Maximum Measure Value', range=[0, max_df['value'].max() * 1.1])  # Adjust the y-axis range to fit max value
+
+                # Save each plot in the specified output directory and show it
+                if output_path:
+                    plot_file_name = f"{dataset}_{measure}_{'pretrained+supervised' if supervised else 'pretrained'}.html"
+                    plot_file = os.path.join(output_path, plot_file_name)
+                    
+                    fig.write_html(plot_file)                                                               # Save as HTML to retain interactivity
+                    
+                    print(f"Saved interactive plot for {measure} in dataset {dataset} at {plot_file}")
+
+                if (show_charts):
+                    fig.show()              # This will display the plot in the notebook or a web browser
+
+
+
+def read_file(file_path):
+
+    df = pd.read_csv(file_path, sep='\t')  # Load data from a CSV file (tab delimited)
+
+    #print("Columns in the file:", df.columns)
+
+    return df
 
 
 
 # ----------------------------------------------------------------------------------------------------------------------------
-#     
+# main()
+#
+# Read arguments and call results_analysis or generate_charts_plotly accordingly 
+# ----------------------------------------------------------------------------------------------------------------------------
 
 def main():
-    if len(sys.argv) < 2:
-        print("Usage: python results_analysis.py <input_file> [output_file]")
-        sys.exit(1)
 
-    input_file = sys.argv[1]
-    output_file = sys.argv[2] if len(sys.argv) > 2 else None
+    parser = argparse.ArgumentParser(description="Analyze model results and generate charts or summaries")
+
+    parser.add_argument('file_path', type=str, help='Path to the CSV file with the data')
+    parser.add_argument('--output_dir', type=str, default='../out', help='Directory to save the output files, default is "../out"')
     
-    #results_analysis(input_file, output_file)
+    parser.add_argument('-c', '--charts', action='store_true', help='Generate charts')
+    parser.add_argument('-s', '--summary', action='store_true', help='Generate summary')
+    
+    parser.add_argument('--show', action='store_true', help='Display charts interactively (requires -c)')
 
-    #generate_charts(input_file, output_file)
+    args = parser.parse_args()
 
-    #generate_charts_seaborn(input_file, output_file)
+    # Ensure at least one operation is specified
+    if not (args.charts or args.summary):
+        parser.error("No action requested, add -c for charts or -s for summary")
 
-    generate_charts_plotly(input_file, output_file)
+    df = read_file(args.file_path)
 
+    if args.summary:
+        results_analysis(df, args.output_dir)
+
+    if args.charts:
+        generate_charts_plotly(df, args.output_dir, show_charts=args.show)
 
 if __name__ == "__main__":
     main()
