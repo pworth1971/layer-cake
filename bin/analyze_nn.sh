84364e1584f2af322628ab93df54ad8b8b750547
Peter J Worth <peterworthjr@gmail.com>
2025-01-15 13:05:59 -0500

    updated analyze script

--- bin/analyze_nn.sh
+++ bin/analyze_nn.sh
@@ -1,1 +1,1 @@
-python ../src/results_analysis.py ../log/jarvis/lc_nn_cuda_all.test.tsv --charts --runtimes --neural --summary --debug
+python ../src/results_analysis.py ../log/lc_nn_hfseq_trans_test.test --neural --charts --runtimes --summary --debug
