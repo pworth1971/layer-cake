9f2a4d30a98f705ff42068bd692000b6f0d0c6c3
Peter Worth <peterworthjr@gmail.com>
2025-01-18 11:52:02 +0000

    hf_

--- bin/trans_lc_hfcnn_test.sh
+++ bin/trans_lc_hfcnn_test.sh
@@ -32,14 +32,14 @@ SEED=77
 
 # Datasets array
 datasets=(
+    "--dataset rcv1"                            # RCV1-v2 (multi-label, 101 classes)
     "--dataset bbc-news"                        # bbc-news (single label, 5 classes)    
     "--dataset reuters21578"                    # reuters21578 (multi-label, 115 classes) 
     "--dataset 20newsgroups"                    # 20newsgroups (single label, 20 classes)
     "--dataset ohsumed"                         # ohsumed (multi-label, 23 classes) 
-    "--dataset imdb"                            # imdb (single-label, 2 classes)     
-    "--dataset arxiv"                           # arxiv (multi-label, 58 classes) 
     "--dataset arxiv_protoformer"               # arxiv_protoformer (single-label, 10 classes)
-    "--dataset rcv1"                            # RCV1-v2 (multi-label, 101 classes)
+    "--dataset arxiv"                           # arxiv (multi-label, 58 classes) 
+    "--dataset imdb"                            # imdb (single-label, 2 classes)     
  )   
 
 # -------------------------------------------------------------------------------
