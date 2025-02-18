0190e8ed392829b22fd4435d81e7d9ea4a24818f
Peter Worth <peterworthjr@gmail.com>
2025-02-18 23:10:42 +0000

    test script update

--- bin/ml_testing.sh
+++ bin/ml_testing.sh
@@ -36,7 +36,7 @@ CONF_MATRIX="--cm"
 
 DATASET_EMB_COMP="--dataset-emb-comp"
 
-declare -a datasets=("imdb" "ohsumed" "arxiv_protoformer" "rcv1" "bbc-news" "reuters21578" "20newsgroups" "arxiv" )
+declare -a datasets=("arxiv_protoformer" "rcv1" "bbc-news" "reuters21578" "20newsgroups" "arxiv" "imdb" "ohsumed")
 declare -a pickle_paths=("../pickles" "../pickles" "../pickles" "../pickles")
 declare -a learners=("svm" "lr" "nb")
 declare -a vtypes=("tfidf" "count")
